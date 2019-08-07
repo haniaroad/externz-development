229ce91953d6dfb58718838d46fd41fac340669a
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2019-08-06 23:20:13 -0400

    Updated json again

--- server/externz/app/controllers/application_controller.rb
+++ server/externz/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+          origins 'http://localhost:3000', 'https://externz.herokuapp.com/' #replace this url with that of your own heroku client app
+          resource '*', :headers => :any, :methods => [:get]
+        end
+      end
 end
