e4aa0078ebb059b818be5be1c058bc1bb0374ad5
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2019-08-07 21:58:49 -0400

    Setting up for Heroku

--- client/Api.js
+++ client/Api.js
@@ -1,19 +1,19 @@
 class API {
   static async login(user) {
-    const response = await fetch(`https://localhost/api/v1/login${user}`);
+    const response = await fetch(`https://git.heroku.com/externz.git/api/v1/login${user}`);
     const data = await response.json();
     console.log(data);
   }
   static async business(business) {
     const response = await fetch(
-      `http://localhost:3000/api/v1/busines${business}`
+      `https://git.heroku.com/externz.git/api/v1/busines${business}`
     );
     const data = await response.json();
     console.log(data);
   }
   static async student(student) {
     const response = await fetch(
-      `http://localhost:3000/api/v1/busines${student}`
+      `https://git.heroku.com/externz.git/api/v1/busines${student}`
     );
     const data = await response.json();
     console.log(data);
