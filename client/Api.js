a79ed87f97b4578bb0aa8086789bd624d729f00c
Derien <haniaroadlifestyle@gmail.comgit config --listgit config --listgit config --global user.name = Deriengit config --global user.email = haniaroadlifestyle@gmail.com>
2019-08-09 00:48:54 -0400

    updated url routes for heroku error log

--- client/Api.js
+++ client/Api.js
@@ -1,19 +1,19 @@
 class API {
   static async login(user) {
-    const response = await fetch(`https://git.heroku.com/externz.git/api/v1/login${user}`);
+    const response = await fetch(`https://externz.herokuapp.com//api/v1/login${user}`);
     const data = await response.json();
     console.log(data);
   }
   static async business(business) {
     const response = await fetch(
-      `https://git.heroku.com/externz.git/api/v1/busines${business}`
+      `https://externz.herokuapp.com//api/v1/busines${business}`
     );
     const data = await response.json();
     console.log(data);
   }
   static async student(student) {
     const response = await fetch(
-      `https://git.heroku.com/externz.git/api/v1/busines${student}`
+      `https://externz.herokuapp.com//api/v1/busines${student}`
     );
     const data = await response.json();
     console.log(data);
